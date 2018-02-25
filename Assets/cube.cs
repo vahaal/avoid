9f5ecbca53ef4d5ea419b88d826ce516ce967b15
c3po\vahaal <damian.zyskowski@o2.pl>
2018-02-25 11:07:43 +0100

    End of beginning

---
+++ Assets/cube.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour {
+    
+    public float delay;
+    public GameObject Cube;
+    public float createRate = 1.5f, createRateTimer;
+    public float rateIncrease = 0.1f, initialCreateDelay = 3.0f;
+    public int callCounter = 0, callsBeforeRateIncrease = 10;
+
+    public Rigidbody cubeRigidBody;
+
+    void Start()
+    {
+        createRateTimer = createRate + initialCreateDelay;
+        cubeRigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        createRateTimer -= Time.deltaTime;
+        if (createRateTimer <= 0)
+        {
+            CustomInvoke();
+        }
+    }
+
+    void CustomInvoke()
+    {
+        Instantiate(Cube, new Vector3(Random.Range(-4,4),15,0),Random.rotation);
+        callCounter++;
+        if (callCounter >= callsBeforeRateIncrease)
+        {
+            createRate -= rateIncrease;
+            callCounter = 0;
+        }
+        createRateTimer = createRate;
+    }
+
+}
+
---
+++ Assets/move.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
